976a2a6636ebdda4cdac054e0f85c1eb6b3efd4e
Haoyu <haoyuwang@unomaha.edu>
2024-02-07 21:34:06 -0600

    web

--- web/main.py
+++ web/main.py
@@ -1,14 +1,20 @@
 import socket
 import uvicorn
+from ec2_metadata import ec2_metadata
 from fastapi import FastAPI
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    return {"message": f"Hello, {ip_address} {hostname}"}
+    return {
+        "message": f"Hello, {ip_address} {hostname}",
+        "metadata": ec2_metadata.__str__()
+    }
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
