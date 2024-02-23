8d51aa54736f097c16c5b9b6c6027c804ada4996
Haoyu <haoyuwang@unomaha.edu>
2024-02-23 02:31:49 -0600

    rds

--- week6/database.py
+++ week6/database.py
@@ -1,15 +1,26 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:123123123@db.cd02ia4k8ew2.us-east-1.rds.amazonaws.com/demo"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    # connect_args={"check_same_thread": False} # this is for sqlite
-)
+if os.environ.get("RDS_HOSTNAME"):
+    hostname = os.environ["RDS_HOSTNAME"]
+    port = os.environ["RDS_PORT"]
+    dbname = os.environ["RDS_DB_NAME"]
+    username = os.environ["RDS_USERNAME"]
+    password = os.environ["RDS_PASSWORD"]
+    SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{username}:{password}@{hostname}/{dbname}"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False}  # this is for sqlite
+    )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
